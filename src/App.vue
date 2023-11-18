be8632e2539f83e259b4c45b1b2854013258e62f
MikePio <michelepiopilla@gmail.com>
2023-11-18 16:27:12 +0100

    Added transitions for component switching Removed the Main component to make it easier to insert transitions for component switching

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <script>
 
 import Header from './components/partials/Header.vue'
-import Main from './components/partials/Main.vue'
 import Footer from './components/partials/Footer.vue'
 
 export default {
   name: 'App',
 
+  data(){
+    return{
+    }
+  },
+
   components:{
     Header,
-    Main,
     Footer
   },
 
 }
+
 </script>
 
 <template>
   <div>
     <Header/>
-    <Main/>
+    <main style="min-height: calc(100vh - 60px - 90px - 150px); max-width: 90%;" class="p-5 mx-auto" >
+      <!-- //* router con le transizioni di quando si passa da una pagina all'altra -->
+      <router-view v-slot="{ Component, route }">
+        <transition name="fade" mode="out-in">
+          <div :key="route.name"> 
+            <component :is="Component" />
+          </div>
+        </transition>
+      </router-view>
+    </main>
     <Footer/>
   </div>
 </template>
 
 <style lang='scss'>
 @use './scss/main.scss' as *;
+body{
+  // todo da eliminare provvisorio
+  // background-color: #4338CA;  
+}
 
+//* transizioni quando si passa da una pagina all'altra
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+//* transizioni quando si passa da una pagina all'altra
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
--- src/components/pages/Contacts.vue
+++ src/components/pages/Contacts.vue
@@ -58,7 +58,7 @@ export default {
         <h2 class="mb-4 text-center">Contatti</h2>
 
         <div v-if="success" class="alert alert-success mt-4" role="alert">
-            <h5 class="text-success p-0 m-0">Form inviato correttamente</h5>
+            <h5 class="text-success p-0 m-0">Mail inviata correttamente</h5>
         </div>        
 
         <div class="d-flex flex-column align-items-center justify-content-center">
